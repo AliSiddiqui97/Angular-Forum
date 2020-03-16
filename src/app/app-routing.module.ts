bbdd2838b4c36d5d0e3b06a2e2e767cd0c9e8617
AliSiddiqui97 <alisiddiqui1997@hotmail.com>
2020-03-16 17:27:10 +0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LandingpageComponent} from '../app/landingpage/landingpage.component';
 import {ProfilepageComponent} from '../app/profilepage/profilepage.component';
+import {ThreadviewComponent} from '../app/threadview/threadview.component'
 
 const routes: Routes = [
   {path:"landingpage", component:LandingpageComponent },
-  {path:"profilepage", component: ProfilepageComponent}
+  {path:"profilepage", component: ProfilepageComponent},
+  {path:"threadviewpage", component: ThreadviewComponent},
+  
 ];
 
 @NgModule({
